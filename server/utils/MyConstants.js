4d19abc3c1b9f8664a35a185451b9ef90fd2f092
PAH911 <phamanhhuy9111@gmail.com>
2024-07-12 16:31:37 +0700

    upload server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster1.mnxtkax.mongodb.net',
+  DB_USER: 'PAH911',
+  DB_PASS: 'Joker911',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'KHOA_BI_MAT',
+  JWT_EXPIRES: '89766761263127312', // in milliseconds
+  EMAIL_USER: 'phamanhhuy9111@gmail.com', // gmail service
+  EMAIL_PASS: 'ihldylmlykkrxhgc'
+};
+module.exports = MyConstants;
